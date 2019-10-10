10be8713c2c20e4ef4cb0c1ac6e13e11927bfa81
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-10-10 19:53:03 -0400

    Added functionality to the buttons to update strikes and balls

--- baseball-app/src/Components/Dashboard.jsx
+++ baseball-app/src/Components/Dashboard.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 
-export default function Dashboard () {
+export default function Dashboard ({ setStrikes, strikes, balls, setBalls, hits, setHits, player, setPlayer }) {
     return (
         <div>
             <h1>Dashboard</h1>
+            
+            <button onClick={() => setStrikes(strikes + 1)}>Add strike</button>
+            {strikes > 3 && (
+                setStrikes(0)
+            )}
+
+            <button onClick={() => setBalls(balls + 1)}>Add Ball</button>
+            {balls > 5 && (
+                setBalls(0)
+            )}
         </div>
     )
 }
--- baseball-app/src/Components/Display.jsx
+++ baseball-app/src/Components/Display.jsx
@@ -2,13 +2,30 @@ import React, { useState } from 'react';
 import Dashboard from './Dashboard';
 
 export default function Display () {
-    const [strikes, setStrikes] = useState();
-    const [balls, setBalls] = useState();
-    const [hits, setHits] = useState();
+    const [strikes, setStrikes] = useState(0);
+    const [balls, setBalls] = useState(0);
+    const [hits, setHits] = useState(0);
+    const [player, setPlayer] = useState('Aaron Spurgeon')
+
     return (
         <div>
             <h1>Display</h1>
-            <Dashboard />
+            <h2>{player} is up to bat.</h2>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <h2 style={{marginRight: '20px'}}>Number of strikes: {strikes}   | </h2>
+                <h2 style={{marginRight: '20px'}}>Number of balls: {balls}   | </h2>
+                <h2 style={{marginRight: '20px'}}>Number of hits: {hits}</h2>
+            </div>
+            <Dashboard
+                setStrikes={setStrikes}
+                strikes={strikes} 
+                balls={balls} 
+                setBalls={setBalls} 
+                hits={hits}
+                setHits={setHits}
+                player={player}
+                setPlayer={setPlayer}
+                />
         </div>
     )
 }
