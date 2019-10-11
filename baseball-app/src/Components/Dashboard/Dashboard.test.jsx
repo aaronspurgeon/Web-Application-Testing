ce2d94e0c3f121929130fdb8cb42b16ba7a9eea3
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-10-10 21:13:16 -0400

    Added in some testing

--- baseball-app/src/Components/Dashboard/Dashboard.test.jsx
+++ baseball-app/src/Components/Dashboard/Dashboard.test.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
-   import * as rtl from '@testing-library/react';
-   import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
    afterEach(rtl.cleanup);
+
+   it('renders the button text', () => {
+       const wrapper = rtl.render(
+           <Dashboard />
+       );
+       const element = wrapper.queryByText(/Set a player to bat!/i);
+       expect(element).toBeVisible();
+   })
